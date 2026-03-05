417d73b7828f1462f96d77cbada40a67392f7765
salmanshabi <149633770+salmanshabi@users.noreply.github.com>
2026-03-05 19:53:58 +0200

    Add real Pipore 1kg image and fix image type declarations

---
+++ src/images.d.ts
@@ -0,0 +1,19 @@
+declare module '/images/*.png' {
+    const value: string;
+    export default value;
+}
+
+declare module '/images/*.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module '/images/*.jpeg' {
+    const value: string;
+    export default value;
+}
+
+declare module '/images/*.svg' {
+    const value: string;
+    export default value;
+}
